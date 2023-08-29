b954d213afb949d0bb922d8a9f694ba81eddcccc
Oyenuga Oluwaseyi Mayowa <110698408+Seyicello@users.noreply.github.com>
2023-08-29 13:54:26 +0100

    quiz app

---
+++ index.js
@@ -0,0 +1,46 @@
+export default [
+{
+    question: "What is my favourite food?",
+    answers: [
+        {option: "Rice and plantain", correct: true},
+        {option: "Fufu and okra soup", correct: false},
+        {option: "Beans and plantain", correct: false},
+    ],
+},
+
+{
+    question: "What do you think is my favourite color?",
+    answers: [
+        {option: "Red", correct: false},
+        {option: "Black", correct: true},
+        {option: "Purple", correct: false},
+    ],
+},
+
+{
+    question: "One is my favorite",
+    answers: [
+        {option: "Movies", correct: false},
+        {option: "Moradeke", correct: true},
+        {option: "Sleeping", correct: false},
+    ],
+},
+
+{
+    question: "What is my surname?",
+    answers: [
+        {option: "Oyenuga", correct: true},
+        {option: "Julius", correct: false},
+        {option: "Itohan", correct: false},
+    ],
+},
+
+{
+    question: "How old am i?",
+    answers: [
+        {option: "25", correct: false},
+        {option: "23", correct: false},
+        {option: "24", correct: true},
+    ],
+},
+]
